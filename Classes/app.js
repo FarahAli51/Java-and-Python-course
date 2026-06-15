620b9fd31bc85a693a29531ba92d7d2a7ba71407
FarahAli51 <farahali4xy@gmail.com>
2026-06-15 12:18:29 +0500

    Assignment

---
+++ Classes/app.js
@@ -0,0 +1,28 @@
+console.log("FullStack Foundation")
+
+
+alert("Hello");
+
+//creating variables
+var class01 = "introduction to programming";
+var class02 = "Javascript";
+var class03 = "HTML and CSS";
+
+console.log(class01);
+console.log(class02);
+console.log(class03);
+
+//editing variables with operators
+
+var num1 = 10;
+var num2 = num1 + 5;
+
+var num3 = 20;
+var num4 = num3++;
+
+console.log(num1++); //post increment answer is 10 but num1 becomes 11
+console.log(num1); //num1 is 11
+console.log(num2);
+console.log(++num3); //pre increment answer is 21 and num3 becomes 21
+console.log(num4);
+
